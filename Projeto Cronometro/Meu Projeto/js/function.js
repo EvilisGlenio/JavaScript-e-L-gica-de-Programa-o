d0e2c3c87f5430e17bf31857ab8a274af5df3124
EvilisGlenio <glenio.developer@gmail.com>
2020-10-02 12:03:08 -0300

    Projeto Cronometro
    Iniciando

---
+++ Projeto Cronometro/Meu Projeto/js/function.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+    var display = document.getElementById('display');
+
+    var hour = document.getElementById('hour');
+    var minute = document.getElementById('minute');
+    var secound = document.getElementById('secound');
+
+    var start = document.getElementById('start');
+
+    var cronometroSeg;
+
+    var horaAtual;
+    var minutoAtual;
+    var segundoAtual;
+
+    var interval;
+
+}
